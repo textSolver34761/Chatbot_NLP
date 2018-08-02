4e4393e22707609d367e82b28d4d13598bc89fe7
Benjamin Pradon <pradonbenjamin@gmail.com>
2018-08-02 18:18:52 +0200

    Tokenizing words and Sentences

---
+++ nlp-v36.py
@@ -0,0 +1,7 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+exemple_text = "Hello Mr. Smith, how are doing today? The weather is great. Python is great."
+
+print(sent_tokenize(exemple_text))
+
+print(word_tokenize(exemple_text))
