6f832051356136b90cd288d43455c5370def2e31
Benjamin Pradon <pradonbenjamin@gmail.com>
2018-08-03 19:07:33 +0200

    stemming

---
+++ stemming.py
@@ -0,0 +1,9 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+
+example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
+
+for w in example_words:
+    print(ps.stem(w))
